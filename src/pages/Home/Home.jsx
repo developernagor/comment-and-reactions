fb89b52a8cba797e1a6db0d50cfcb5343a78b020
developernagor <developernagor@gmail.com>
2025-03-21 23:19:20 +0600

    Added features section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './Hero'
+import Features from './Features'
 
 function Home() {
   return (
     <div>
       <Hero></Hero>
+      <Features></Features>
     </div>
   )
 }
